eaf50ecd7702207b9ab7ab6104deef6d10b30930
Taufiq Mohammed <taufiqmmhd@gmail.com>
2020-03-27 15:19:41 +0800

    feat: maintain 1 otp entry for each number & store salt
    This commit makes sure that there's only one OTP entry for each contact
    number and that it gets updated everytime a user generates a new OTP.
    
    The entry now also stores the salt so that we can check if the OTP
    matches when verifying.

--- app/index.js
+++ app/index.js
@@ -13,8 +13,6 @@ app.use(express.json());
 
 // OTP crypto
 const SALT_ROUNDS = 10;
-const { OTP_SALT } = process.env;
-
 
 // DynamoDB constants
 const HOMER_OTP_TABLE = 'homer-native-otp';
@@ -27,6 +25,7 @@ const homerAgencies = ['mom-sho', 'ica-homer', 'moe-homer', 'mom-dloa', 'mom-fdw
 
 async function findUser(contactNumber) {
   // Searches the contact number in every agency's user table
+  // TODO we can actually use `transactGet` instead - https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#transactGet-property
   for (let i = 0; i < homerAgencies.length; i += 1) {
     const agency = homerAgencies[i];
     const userTable = `${agency}-users-${NODE_ENV}`;
@@ -54,19 +53,29 @@ async function createOTP(contactNumber) {
   }
   while (otp.match(/[a-z]/i));
 
+  // TODO added this log so that I can see the OTP and test if verification works
+  // hide the OTP when it goes into prod
+  console.log('created OTP', otp, ' for contact: ', contactNumber);
   // 2. Save OTP
-  const hashedOtp = bcrypt.hashSync(OTP_SALT, SALT_ROUNDS);
-  const putParams = {
-    Item: {
-      contact_number: contactNumber,
-      updated_time: new Date().getTime().toString(),
-      hashed_otp: hashedOtp,
-    },
+  const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+  const hashedOtp = bcrypt.hashSync(otp, salt);
+
+  // 2.2 Create/Update OTP entry
+  const updateParams = {
     TableName: HOMER_OTP_TABLE,
+    Key: {
+      contact_number: contactNumber,
+    },
+    UpdateExpression: 'set updated_time = :updatedTime, hashed_otp = :hashedOtp, salt = :salt',
+    ExpressionAttributeValues: {
+      ':hashedOtp': hashedOtp,
+      ':updatedTime': new Date().getTime(),
+      ':salt': salt,
+    },
   };
 
   try {
-    await docClient.put(putParams).promise();
+    await docClient.update(updateParams).promise();
   } catch (error) {
     throw new Error(`Error saving OTP to ${HOMER_OTP_TABLE}: ${error.message}`);
   }
@@ -78,6 +87,7 @@ app.post('/otp', async (req, res) => {
   const user = await findUser(contactNumber);
   if (!user) {
     res.status(404).send('Number not found');
+    return;
   }
 
   // 2. Create entry with OTP Hash [number, agency, otp, time]
