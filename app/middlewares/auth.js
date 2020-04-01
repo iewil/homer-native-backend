e84689c94a92462f4555f19b8a9e5588e487d567
Taufiq Mohammed <taufiqmmhd@gmail.com>
2020-04-01 18:27:48 +0800

    feat(auth): return 401 for expired tokens

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -15,14 +15,20 @@ function checkToken(req, res, next) {
   try {
     const user = jwt.verify(signedToken, TOKEN_SIGNING_KEY);
     req.user = user;
+    next();
   } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      res.status(401).send('Token expired');
+      return;
+    }
     if (err instanceof jwt.JsonWebTokenError) {
       res.status(400).send('Malformed JWT');
+      return;
     }
+
     console.log('Error authenticating', err, JSON.stringify(req.headers));
     res.status(500).send('Unhandled error, check logs');
   }
-  next();
 }
 
 module.exports = checkToken;
