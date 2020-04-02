0de133f52b9264716e826f8826acbb66c9aa835a
Taufiq Mohammed <taufiqmmhd@gmail.com>
2020-04-02 12:55:46 +0800

    feat(user): use constant that stores table names when finding a user
    Previously when finding a user, we had an array of agency prefixes
    (`ica-homer`, `homer`, `mom-dloa`, etc.), where we'd loop through them
    and appended `-users-<table_env>` to it before making a query to find
    the user.
    
    This commit now introduces an array with all the users table already
    constructed (`ALL_HOMER_USERS_TABLE`). Now, when finding a user, we do
    not need to construct the table from the agency prefix and can just loop
    through that above-mentioned constant instead.

--- app/db/constants.js
+++ app/db/constants.js
@@ -4,7 +4,6 @@ const HOMER_NATIVE_OTP_TABLE = `homer-native-otp-${TABLE_ENV}`;
 const HOMER_NATIVE_LOCATIONS_TABLE = `homer-native-locations-${TABLE_ENV}`;
 const HOMER_NATIVE_USERS_TABLE = `homer-native-users-${TABLE_ENV}`;
 
-// TODO figure out integrating this into `findUser` function
 const ALL_HOMER_USERS_TABLE = ['homer-native', 'mom-sho', 'ica-homer', 'moe-homer', 'mom-dloa', 'mom-fdw', 'mom-sho', 'homer'].map((prefix) => `${prefix}-users-${TABLE_ENV}`);
 
 module.exports = {
--- app/helpers/user.js
+++ app/helpers/user.js
@@ -5,20 +5,27 @@ const { HOMER_NATIVE_USERS_TABLE, ALL_HOMER_USERS_TABLE } = require('../db');
 // Error imports
 const { UserNotFoundError } = require('../errors/UserErrors');
 
-const { TABLE_ENV } = process.env;
 const AWS_REGION_NAME = 'ap-southeast-1';
 AWS.config.update({ region: AWS_REGION_NAME });
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-// Agencies
-const homerAgencies = ['homer-native', 'mom-sho', 'ica-homer', 'moe-homer', 'mom-dloa', 'mom-fdw', 'mom-sho', 'homer'];
-
+/**
+ * This goes through every users table under the Homer SMS system to find
+ * if the users' contact number is in any of the tables
+ * @param {String} contactNumber User's contact number
+ * @returns {Object}  user                 user info
+ * @returns {String}  user.contactNumber   user's contactNumer
+ * @returns {String}  user.agency          agency enforcing user's SHN
+ *
+ * ### Why users are stored in multiple tables
+ * The SMS system groups users based on the agency that enforces their Stay-Home Notice (SHN),
+ * and the tables are named accordingly
+ * (e.g the `ica-homer-users-production` table stores users who's SHNs are enforced by ICA)
+ */
 async function findUser(contactNumber) {
-  // Searches the contact number in every agency's user table
   // TODO we can actually use `transactGet` instead - https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#transactGet-property
-  for (let i = 0; i < homerAgencies.length; i += 1) {
-    const agency = homerAgencies[i];
-    const userTable = `${agency}-users-${TABLE_ENV}`;
+  for (let i = 0; i < ALL_HOMER_USERS_TABLE.length; i += 1) {
+    const userTable = ALL_HOMER_USERS_TABLE[i];
     const params = {
       TableName: userTable,
       Key: {
@@ -28,17 +35,21 @@ async function findUser(contactNumber) {
     try {
       const result = await docClient.get(params).promise();
       if (!_.isEmpty(result)) {
+        // If we found the user's record in our native app users table, the agency that's
+        // enforcing the user's SHN is already stored alongside their contact number (`agency`),
+        // so we can just retrieve the agency info from the entry we found
         if (userTable === HOMER_NATIVE_USERS_TABLE) {
           return { contactNumber, agency: result.Item.agency };
         }
+
+        // This is to find the agency prefix of the table
+        // (i.e get `ica-homer` from `ica-homer-users-staging`)
+        // so we can keep track of which agency is enforcing the person's SHN compliance
+        const agency = userTable.slice(0, userTable.indexOf('users') - 1);
         return { contactNumber, agency };
       }
     } catch (error) {
-      if (error.name === 'ResourceNotFoundException') {
-        console.log(`${userTable} is non-existent during search with ${contactNumber}`);
-      } else {
-        console.log(`AWS error finding ${contactNumber} in ${userTable} ${error}`);
-      }
+      console.log(`AWS error finding ${contactNumber} in ${userTable} ${error}`);
     }
   }
   throw new UserNotFoundError(contactNumber);
