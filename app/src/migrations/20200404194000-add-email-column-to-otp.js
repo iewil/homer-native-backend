0e4216ae50b406774ed66c9687b4b7434b3b7da5
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2020-04-04 19:57:41 +0800

    fix: update OTP model and add migration
    This commit updates the OTP model to include a nullable field, `email`, as well
    as change the existing `contact_number` field to be nullable. The commit also
    includes the necessary migrations for the aforementioned changes.

---
+++ app/src/migrations/20200404194000-add-email-column-to-otp.js
@@ -0,0 +1,48 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+    (t) => Promise.all([
+      queryInterface.changeColumn(
+        'Otp', // name of Source model
+        'contact_number', // name of the key we're modifying
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        }, { transaction: t },
+      ),
+      queryInterface.addColumn(
+        'Otp', // name of Source model
+        'email', // name of the key we're adding
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        }, { transaction: t },
+      ),
+    ]),
+  ),
+
+  down: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+    (t) => Promise.all(
+      [
+        queryInterface.changeColumn(
+          'Otp', // name of Source model
+          'contact_number', // key we want to remove
+          {
+            type: Sequelize.STRING,
+            allowNull: false,
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          }, { transaction: t },
+        ),
+        queryInterface.removeColumn(
+          'Otp', // name of Source model
+          'email', // key we want to remove
+          { transaction: t },
+        ),
+      ],
+    ),
+  ),
+};
