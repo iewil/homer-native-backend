dd7e05f0443028dbd290e1b8a51706e77eb2654f
Taufiq Mohammed <taufiqmmhd@gmail.com>
2020-04-04 12:41:55 +0800

    feat(route): complete otp /generate /verify endpoints

--- app/routes/otp.js
+++ app/routes/otp.js
@@ -1,30 +1,35 @@
 // Imports
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const bcrypt = require('bcryptjs');
 
-async function generateOtp (req, res) {
+const router = express.Router();
+const OtpService = require('../services/OtpService');
+
+async function generateOtp(req, res) {
   const { contact_number: contactNumber } = req.body;
   try {
-    // TO-DO
-    let otp
-    res.status(200).send({ otp })
+    await OtpService.createOtp(contactNumber);
+    res.status(200).send({ message: 'OTP created' });
   } catch (err) {
-    res.status(500).send(err.message)
+    res.status(500).send(err.message);
   }
-};
+}
 
-async function verifyOtp (req, res) {
+async function verifyOtp(req, res) {
   const { contact_number: contactNumber, otp } = req.body;
   try {
-    // TO-DO
-    let accessToken
-    res.status(200).send({ access_token: accessToken })
+    const retrievedOtp = await OtpService.getOtp(contactNumber);
+    if (bcrypt.compareSync(otp, retrievedOtp)) {
+      res.status(200).send({ access_token: 'Access token!' });
+    } else {
+      res.status(400).send('Invalid OTP');
+    }
   } catch (err) {
-    res.status(500).send(err.message)
+    res.status(500).send(err.message);
   }
-};
+}
 
-router.post('/generate', generateOtp)
-router.post('/verify', verifyOtp)
+router.post('/generate', generateOtp);
+router.post('/verify', verifyOtp);
 
 module.exports = router;
