8480ab54ed420e6d83212ba9e2b63ccbc39e1d2d
kwajiehao <31984694+kwajiehao@users.noreply.github.com>
2020-04-04 19:39:48 +0800

    Feat/photos route (#1)
    * chore: install dependencies
    
    * feat: added presigned URL generation endpoint
    
    * chore: replace empty const declarations
    
    * fix: put BUCKET_NAME as an env var
    
    * chore: add apidoc info
    
    * fix: rename S3 photo bucket name env var

--- app/index.js
+++ app/index.js
@@ -48,8 +48,8 @@ app.use(express.json({ limit: '10mb'}));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-  'origin': '*',
-  'credentials': true,
+  origin: '*',
+  credentials: true,
 }));
 
 // Protected routes
