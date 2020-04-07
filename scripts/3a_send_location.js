dc2b6abc818de55fc995d8235b115f846355be3f
Pallani Kumaran <pallani@open.gov.sg>
2020-04-07 14:17:16 +0800

    Fix bugs in send script

--- scripts/3a_send_location.js
+++ scripts/3a_send_location.js
@@ -13,7 +13,7 @@ const PN_MESSAGE = {
     // Set to how long parking command would still be rewlevant
     // Currently 5 mins
     ttl: 60 * 60 * 1000, // milliseconds (60 minutes)
-    restrictedPackageName: 'sg.parking.streetsmart', // This ensures message is only sent to this app
+    restrictedPackageName: 'sg.gov.homer', // This ensures message is only sent to this app
     // collapseKey serves as an identifier for a group of messages that can be collapsed
     // so that only the last message gets sent when delivery can be resumed.
     // A maximum of four different collapse keys may be active at any given time.
@@ -23,7 +23,7 @@ const PN_MESSAGE = {
   notification: {
     title: 'Singapore Government',
     body: 'Click here to complete your location reporting',
-  }
+  },
   data: {
     action: 'getLocation',
   },
@@ -31,7 +31,7 @@ const PN_MESSAGE = {
 
 const SMS_MESSAGE = 'Please submit your location on the Homer App'
 
-async function main ({ firebaseCredentialFilePath, twilioId, twilioToken, twilioNumber, pushNotificationFilePath, smsFilePath }) {
+async function main ({ firebaseCredentialFilePath, twilioId, twilioToken, twilioAlphaSender, twilioMessagingServiceId, pushNotificationFilePath, smsFilePath }) {
 
   console.log(`Reading PNs from ${pushNotificationFilePath}...`)
   const pns = jsonfile.readFileSync(pushNotificationFilePath)
@@ -50,7 +50,8 @@ async function main ({ firebaseCredentialFilePath, twilioId, twilioToken, twilio
     pns,
     pn => {
       console.log(`sending pn for ${pn.qo}`)
-      return messaging
+      console.log({...PN_MESSAGE, token: pn.pn })
+      return fcm
         .send({...PN_MESSAGE, token: pn.pn })
         .catch(e => console.log(`error while sending : ${e.name}`))
     }
@@ -69,10 +70,11 @@ async function main ({ firebaseCredentialFilePath, twilioId, twilioToken, twilio
   await bluebird.mapSeries(
     smses,
     sms => {
-      console.log(`sending sms for ${pn.qo}`)
+      console.log(`sending sms for ${sms.qo}`)
       return twilio.messages.create({
         body: SMS_MESSAGE,
-        from: twilioNumber,
+        messagingServiceSid: twilioMessagingServiceId,
+        from: twilioAlphaSender,
         to: `+${sms.phone}`
       })
       .catch(e => console.log(`error while sending : ${e.name}`))
@@ -88,7 +90,8 @@ async function main ({ firebaseCredentialFilePath, twilioId, twilioToken, twilio
 program.requiredOption('--firebaseCredentialFilePath <firebaseCredentialFilePath>', 'Firebase Credentials JSON filename')
 program.requiredOption('--twilioId <twilioId>', 'Twilio ID')
 program.requiredOption('--twilioToken <twilioToken>', 'Twilio Token')
-program.requiredOption('--twilioNumber <twilioNumber>', 'Twilio Number')
+program.requiredOption('--twilioAlphaSender <twilioAlphaSender>', 'Twilio Alpha Sender')
+program.requiredOption('--twilioMessagingServiceId <twilioMessagingServiceId>', 'Twilio Messaging Service Id')
 program.requiredOption('--pushNotificationFilePath <pushNotificationFilePath>', 'Push Notification JSON filename')
 program.requiredOption('--smsFilePath <smsFilePath>', 'SMS JSON filename')
 program.parse(process.argv)
